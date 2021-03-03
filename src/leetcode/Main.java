9880a63b1e0e6125e0ae01074f949a7d18042b5c
solo1020 <1819121506@qq.com>
2021-03-03 21:50:31 +0800

    add leetcode 1047

---
+++ src/leetcode/Main.java
@@ -0,0 +1,31 @@
+package leetcode;
+
+import java.util.Scanner;
+
+/**
+ * @ClassName Main
+ * @description:
+ * @author: isquz
+ * @time: 2021/3/3 21:44
+ */
+public class Main {
+    public static void main(String[] args) {
+
+        Scanner in = new Scanner(System.in);
+
+
+    }
+
+    public static void delSameChar(String s){
+        for(int i = 0; i < s.length()-1; i++){
+            if(s.charAt(i) == s.charAt(i+1)){
+                if(i+1 == s.length() - 2){
+
+                }
+            }
+        }
+    }
+
+
+
+}
